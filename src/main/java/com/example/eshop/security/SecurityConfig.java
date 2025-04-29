369e7b9153d32401b0f17160d70deb97f46c11bf
Tsuniti <Tsuniti00@gmail.com>
2025-04-29 17:18:48 +0200

    fix adding a user role to access management pages

--- src/main/java/com/example/eshop/security/SecurityConfig.java
+++ src/main/java/com/example/eshop/security/SecurityConfig.java
@@ -19,82 +19,83 @@ import org.springframework.security.web.SecurityFilterChain;
 //SecurityConfig
 @Configuration
 public class SecurityConfig {
-	
+
 	@Bean
-	public PasswordEncoder passwordEncoder(){
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	//!!! @Lazy
 	@Lazy
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
+
 	//1 Авторизація в пам'яті (тестовий варіант)
 	//@Bean
 	public UserDetailsService userDetailsService() {
 		UserDetails admin = User
-			 .withUsername("a")
-			 .password(passwordEncoder.encode("a"))
-			 .authorities(UserEntity.Role.ADMIN.name())
-			 .build();
+				.withUsername("a")
+				.password(passwordEncoder.encode("a"))
+				.authorities(UserEntity.Role.ADMIN.name())
+				.build();
 		UserDetails user = User
-			 .withUsername("u")
-			 .password(passwordEncoder.encode("u"))
-			 .authorities(UserEntity.Role.USER.name())
-			 .build();
+				.withUsername("u")
+				.password(passwordEncoder.encode("u"))
+				.authorities(UserEntity.Role.USER.name())
+				.build();
 		return new InMemoryUserDetailsManager(admin, user);
 	}
-	
+
 	//Безпека, дозволи, переходи
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
 		return security
-			 /*.headers(headers -> headers
-				  // Вимикає кешування
-				  .cacheControl(cache -> cache.disable())
-			 )*/
-			 //Налаштування дозволених адрес
-			 .authorizeHttpRequests(request ->
-				  request
-						//Дозволені сторінки .permitAll()
-						.requestMatchers("/", "/error", "/welcome", "/register", "/register-form", "/*.css").permitAll()
-						//Дозволені сторінки залежно від повноважень
-						//функції .has...()
-						.requestMatchers("/users", "/products", "/categories").hasAuthority(
-							 UserEntity.Role.ADMIN.name()
-						)
-						//Дозволити запити автентифікованих користувачів
-						.anyRequest().authenticated()
-			 )
-			 //Встановлення і дозвіл для стандартної сторінки авторизації
-			 .formLogin(formLogin ->
-				  formLogin
-						.successForwardUrl("/")
-						.permitAll()
-			 )
-			 //Встановлення і дозвіл для стандартної адреси і сторінки виходу
-			 //адреса /logout
-			 .logout(logoutConfigurer ->
-				  logoutConfigurer
-						//.logoutSuccessUrl("/welcome")
-						.logoutSuccessUrl("/")
-						.permitAll()
-			 )
-			 //+++
-			 //Встановлення власної сторінки авторизації
-			 //по адресі /authorization
-			 /*.formLogin(formLoginConfigurer ->
-				  formLoginConfigurer
-						.loginPage("/authorization").permitAll()
-			 )*/
-			 //Встановлення власної сторінки виходу
-			 /*.logout(logoutConfigurer ->
-				  logoutConfigurer
-						.logoutUrl("/logoutown").permitAll()
-			 )*/
-			 //
-		    //Створення об'єкту SecurityFilterChain
-			 .build();
+				/*.headers(headers -> headers
+					 // Вимикає кешування
+					 .cacheControl(cache -> cache.disable())
+				)*/
+				//Налаштування дозволених адрес
+				.authorizeHttpRequests(request ->
+						request
+								//Дозволені сторінки .permitAll()
+								.requestMatchers("/", "/error", "/welcome", "/register", "/register-form", "/*.css").permitAll()
+								//Дозволені сторінки залежно від повноважень
+								//функції .has...()
+								.requestMatchers("/users", "/products", "/categories").hasAnyAuthority(
+										UserEntity.Role.ADMIN.name(),
+										UserEntity.Role.USER.name()
+								)
+								//Дозволити запити автентифікованих користувачів
+								.anyRequest().authenticated()
+				)
+				//Встановлення і дозвіл для стандартної сторінки авторизації
+				.formLogin(formLogin ->
+						formLogin
+								.successForwardUrl("/")
+								.permitAll()
+				)
+				//Встановлення і дозвіл для стандартної адреси і сторінки виходу
+				//адреса /logout
+				.logout(logoutConfigurer ->
+						logoutConfigurer
+								//.logoutSuccessUrl("/welcome")
+								.logoutSuccessUrl("/")
+								.permitAll()
+				)
+				//+++
+				//Встановлення власної сторінки авторизації
+				//по адресі /authorization
+				/*.formLogin(formLoginConfigurer ->
+					 formLoginConfigurer
+						   .loginPage("/authorization").permitAll()
+				)*/
+				//Встановлення власної сторінки виходу
+				/*.logout(logoutConfigurer ->
+					 logoutConfigurer
+						   .logoutUrl("/logoutown").permitAll()
+				)*/
+				//
+				//Створення об'єкту SecurityFilterChain
+				.build();
 	}
 }
